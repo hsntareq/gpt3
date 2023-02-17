668b30b91dd9476ac09635a2fc64260fd33e404a
Hasan Tareq <hsntareq@gmail.com>
2023-02-17 15:48:24 +0600

    image alt added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -18,7 +18,7 @@ function Navbar() {
         <div className="gpt3__navbar">
             <div className="gpt3__navbar-links">
                 <div className="gpt3__navbar-links_logo">
-                    <img src={logo} />
+                    <img src={logo} alt='logo' />
                 </div>
                 <div className="gpt3__navbar-links_container">
                     <Menu />
